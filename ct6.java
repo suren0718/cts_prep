70dfc24cef209921049db418b22b0dde2e2aa1d2
Surendran S <surendransivakumar2004@gmail.com>
2026-01-19 22:10:24 +0530

    Implement prime number check in ct6.java
    Check if a number is prime and print the result.

---
+++ ct6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ct6{
+  public static void main(String args[]){
+    Scanner raghulan = new Scanner(System.in);
+    int n = raghulan.nextInt();
+    for (int i=2; i<n; i++){
+      if(n%i==0){
+        System.out.println("Not a prime number");
+        break;
+      }
+      else{
+        System.out.println("Prime number");
+        break;
+      }
+    }
+  
+  }
+}
